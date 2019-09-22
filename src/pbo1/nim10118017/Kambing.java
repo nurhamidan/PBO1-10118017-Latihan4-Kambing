95bb33b6e11e2f2e30a06cd24e64ff38c78c8e12
Agung Nurhamidan <>
2019-09-22 17:15:30 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Kambing.java
@@ -0,0 +1,23 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Mengoperasikan variabel lokal jumlahKambing dan menampilkannya di layar.
+ * 
+ */
+public class Kambing {
+    
+    public void tambahKambing() {
+        //Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        
+        System.out.println("Jumlah kambing setelah ditambah\t: " + jumlahKambing);
+    }
+    
+}
---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,23 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Memanggil method yang ada pada class Kambing.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        
+        kambingJantan.tambahKambing();
+    }
+    
+}
